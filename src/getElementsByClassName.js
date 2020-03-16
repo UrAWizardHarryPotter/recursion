d6d388a6e7dc67abfacb1c22e4b88433e6d5743b
Zachary Schwartzman <zach.schwartzman@gmail.com>
2020-03-16 01:55:54 -0700

    Add lostChildren funtion and made fun story to return theLostBoys

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,14 +5,19 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
-  // assign empty array to domFamily
-  // declare lostChildren function
-    // assign arguments parameter to someNode
-    // if someNode's classList contains the className
-      // push someNode into domArray
-    // if someNode has children
-      // call lostChildren on someNode's children
-
+  // assign empty array to theLostBoys
+  let theLostBoys = [];
+  // declare lostChildren function with neverland parameter
+  function lostChildren(neverland) {
+    // assign neverland to peterPan
+    let peterPan = neverland;
+    // if on peterPan's classList and contains the className, push peterPan into theLostBoys
+    if (peterPan.classList && peterPan.classList.contains(className)) {theLostBoys.push(peterPan);}
+    // if peterPan has childNodes, recurse the lostChildren forEach
+    if (peterPan.childNodes) {peterPan.childNodes.forEach(lostChildren);}
+  }
 // invoke lostChildren on the document.body
-// return domFamily
+lostChildren(document.body);
+// return theLostBoys
+return theLostBoys;
 };
