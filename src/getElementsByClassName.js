ee9d68ff7cd9cb7c5ceffc1e37b2760d7c26e6df
Zachary Schwartzman <zach.schwartzman@gmail.com>
2020-03-16 01:17:35 -0700

    Add psuedocode to find lostChildren of domFamily

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,15 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // assign empty array to domFamily
+  // declare lostChildren function
+    // assign arguments parameter to someNode
+    // if someNode's classList contains the className
+      // push someNode into domArray
+    // if someNode has children
+      // call lostChildren on someNode's children
+
+// invoke lostChildren on the document.body
+// return domFamily
 };
